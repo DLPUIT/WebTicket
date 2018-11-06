ab6a0fed9600d8c6cca80ad086b96f7eeff6e32a
ike <wxy1253291481@outlook.com>
2018-11-06 16:16:08 +0800

    cookie

--- WebTicket/Controllers/UserController.cs
+++ WebTicket/Controllers/UserController.cs
@@ -7,11 +7,15 @@ using WebTicket.Repo;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web.Security;
+using Newtonsoft.Json;
 
 namespace WebTicket.Controllers
 {
     public class UserController : Controller
     {
+
+
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
@@ -22,30 +26,111 @@ namespace WebTicket.Controllers
 
             return View();
         }
-        public ActionResult LoginData(UserModel newUser)
+        public ActionResult LoginData(UserModel newUser, string returnUrl)
         {
-            MD5 md5 = new MD5CryptoServiceProvider();
-            byte[] originalBytes = Encoding.Default.GetBytes(newUser.Password);
-            byte[] encodedBytes = md5.ComputeHash(originalBytes);
-            newUser.Password = BitConverter.ToString(encodedBytes);
-            var handler = new UserRepo();
-            var user = handler.Get(x => x.RuiJieId == newUser.RuiJieId);
-            if (user.Password == newUser.Password)
+            if (this.ModelState.IsValid)
             {
-                FormsAuthentication.RedirectFromLoginPage(newUser.Name, newUser.IsRememberMe);
-                return RedirectToAction("index", "User");
+                //newUser.Name = newUser.Name.Trim();
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] originalBytes = Encoding.Default.GetBytes(newUser.Password);
+                byte[] encodedBytes = md5.ComputeHash(originalBytes);
+                newUser.Password = BitConverter.ToString(encodedBytes);
+                var handler = new UserRepo();
+                var user = handler.Get(x => x.RuiJieId == newUser.RuiJieId);
+                if (user.Password == newUser.Password)
+                {
+                    FormsAuthentication.SetAuthCookie(newUser.Name, newUser.IsRememberMe);
+                    return this.CheckReturnUrl(returnUrl)
+                        ? this.Redirect(returnUrl)
+                        : this.RedirectToAction("index", "User") as ActionResult;
+                }
+                this.ModelState.AddModelError("", "请检查你的用户名或密码");
             }
-            else
-            {
-                return RedirectToAction("error", "User");
-            }
-
+            return this.View("Index", newUser);
         }
-        public ActionResult SignOut()
+
+        public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
-            return RedirectToAction("index", "User");
+            return this.RedirectToAction("Index");
         }
+
+        private static void SetAuthCookie(
+            string userName,
+            bool createPersistentCookie,
+            object userData)
+        {
+            if (!System.Web.HttpContext.Current.Request.IsSecureConnection && FormsAuthentication.RequireSSL)
+            {
+                throw new HttpException("Connection not secure creating secure cookie");
+            }
+
+            // <!> In this way, we will lose the function of cookieless
+            //var flag = UseCookieless(
+            //    System.Web.HttpContext.Current,
+            //    false,
+            //    FormsAuthentication.CookieMode);
+
+            FormsAuthentication.Initialize();
+            if (userName == null)
+            {
+                userName = string.Empty;
+            }
+            var cookiePath = FormsAuthentication.FormsCookiePath;
+            var utcNow = DateTime.UtcNow;
+            var expirationUtc = utcNow + FormsAuthentication.Timeout;
+            var authenticationTicket = new FormsAuthenticationTicket(
+                2,
+                userName,
+                utcNow.ToLocalTime(),
+                expirationUtc.ToLocalTime(),
+                createPersistentCookie,
+                JsonConvert.SerializeObject(userData),
+                cookiePath
+            );
+
+            var encryptedTicket = FormsAuthentication.Encrypt(authenticationTicket);
+            if (string.IsNullOrEmpty(encryptedTicket))
+            {
+                throw new HttpException("Unable to encrypt cookie ticket");
+            }
+            var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket)
+            {
+                HttpOnly = true,
+                Path = cookiePath,
+                Secure = FormsAuthentication.RequireSSL
+            };
+
+            if (FormsAuthentication.CookieDomain != null)
+            {
+                authCookie.Domain = FormsAuthentication.CookieDomain;
+            }
+            if (authenticationTicket.IsPersistent)
+            {
+                authCookie.Expires = authenticationTicket.Expiration;
+            }
+
+            System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
+        }
+
+        private bool CheckReturnUrl(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                return false;
+            }
+            // Make Sure the return url was not redirect to an external site.
+            if (Uri.TryCreate(url, UriKind.Absolute, out var absoluteUri))
+            {
+                return string.Equals(
+                    this.Request.Url.Host,
+                    absoluteUri.Host, StringComparison.OrdinalIgnoreCase);
+            }
+            return url[0] == '/' && (url.Length == 1
+                                     || url[1] != '/' && url[1] != '\\')
+                   || url.Length > 1 && url[0] == '~' && url[1] == '/';
+        }
+
         public ActionResult Register()
         {
             return View();
@@ -53,12 +138,12 @@ namespace WebTicket.Controllers
 
         public ActionResult SaveRegisterDate(UserModel newUser)
         {
-            
+
             MD5 md5 = new MD5CryptoServiceProvider();
             byte[] originalBytes = Encoding.Default.GetBytes(newUser.Password);
             byte[] encodedBytes = md5.ComputeHash(originalBytes);
-            newUser.Password=BitConverter.ToString(encodedBytes);
-            newUser.ConfirmPassword=BitConverter.ToString(encodedBytes);
+            newUser.Password = BitConverter.ToString(encodedBytes);
+            newUser.ConfirmPassword = BitConverter.ToString(encodedBytes);
             var handler = new UserRepo();
             handler.Add(newUser);
             return RedirectToAction("Login", "User");
